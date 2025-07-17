5a3d5b1cd4ed059e5aa6992c37971d9dc272c247
mak-43 <letsmailmak@gmail.com>
2025-07-17 12:59:32 +0600

    div create and data insert into div

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 function bringData(){
+
+    const container=document.querySelector('.data-container')
+
    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((response)=>console.log(response))
+    .then((response)=>{   
+        console.log(response)
+        return response.json()
+    }).then( (data)=>{
+        data.forEach(element => {
+            
+            const titleDiv=document.createElement('div')
+            const idDiv=document.createElement('div')
+            const bodyDiv=document.createElement('div')
+            
+
+            titleDiv.innerText=element.title
+            idDiv.innerText=element.id
+            bodyDiv.innerText=element.body
+
+            container.appendChild(idDiv)
+            container.appendChild(bodyDiv)
+            container.appendChild(titleDiv)
+            console.log(element)
+        });
+    })
   
 
 }
